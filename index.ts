7550adab3cfcff5fad016605a598f884704f9b40
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-01-09 21:36:22 +0100

    First commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import Express, { Request, Response } from "express";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+
+const app = Express();
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send({message: "Welcome to the index"})
+// })
+
+passport.use(new GoogleStrategy()); //generic register of strategies
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
