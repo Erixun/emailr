f7627443c8b57e5fe5a17533539c014839b55ca9
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-01-10 21:57:47 +0100

    import keys

--- index.ts
+++ index.ts
@@ -3,14 +3,22 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import * as dotenv from "dotenv";
 dotenv.config();
+import * as keys from "./config/keys.js";
 
+console.log(keys.GOOGLE_CLIENT_ID);
 const app = Express();
 
 // app.get('/', (req: Request, res: Response) => {
 //   res.send({message: "Welcome to the index"})
 // })
-
-// passport.use(new GoogleStrategy()); //generic register of strategies
+// const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+passport.use(new GoogleStrategy({
+  clientID: keys.GOOGLE_CLIENT_ID,
+  clientSecret: keys.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken)
+}); //generic register of strategies
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
