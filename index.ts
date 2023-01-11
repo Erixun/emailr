e4814f9830e98e9788a5d15e66bee7e2035c3998
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-01-11 21:41:02 +0100

    setup google auth route

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import * as dotenv from "dotenv";
 dotenv.config();
-import * as keys from "./config/keys.js";
+import keys from "./config/keys.js";
 
 console.log(keys.GOOGLE_CLIENT_ID);
 const app = Express();
@@ -12,13 +12,26 @@ const app = Express();
 //   res.send({message: "Welcome to the index"})
 // })
 // const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
-passport.use(new GoogleStrategy({
-  clientID: keys.GOOGLE_CLIENT_ID,
-  clientSecret: keys.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-  console.log(accessToken)
-}); //generic register of strategies
+passport.use(
+  new GoogleStrategy( //I am known as a strategy called 'google'
+    {
+      clientID: keys.GOOGLE_CLIENT_ID,
+      clientSecret: keys.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+); //generic register of strategies
+
+app.get(
+  //ERROR: 400: redirect_uri_mismatch ?
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"], //what access we want to have, from a (key)list of scopes
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
