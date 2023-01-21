e99ac177f2f93bcb1fdc1a68e7085de12a994ade
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-01-21 21:46:49 +0100

    implement cookieSession

--- index.ts
+++ index.ts
@@ -5,7 +5,21 @@ import authRoutes from "./routes/authRoutes.js";
 import user from "./models/User.js";
 import "./services/passport.js";
 import { connect } from "mongoose";
+import passport from "passport";
+import cookieSession from "cookie-session";
 const app = Express();
+
+const cookieKey = process.env.COOKIE_KEY;
+if (typeof cookieKey === undefined) throw Error("KEY UNAVAILABLE");
+app
+  .use(
+    cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [cookieKey as string],
+    })
+  )
+  .use(passport.initialize())
+  .use(passport.session());
 
 const MONGO_URI = process.env.MONGO_URI ?? "";
 console.log(MONGO_URI);
