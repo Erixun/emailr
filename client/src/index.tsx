23f69b27cdd626ec7e0276b12dda25580f2bfded
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-02-04 20:12:15 +0100

    redux setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,47 @@
 //Sets up data layer of our application (Redux)
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
 import App from "./components/App";
+interface CounterState {
+  value: number;
+}
+
+const increment = createAction("counter/increment");
+const decrement = createAction("counter/decrement");
+const incrementByAmount = createAction<number>("counter/incrementByAmount");
+
+const initialState = { value: 0 } as CounterState;
+
+const counterReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(increment, (state, action) => {
+      state.value++;
+    })
+    .addCase(decrement, (state, action) => {
+      state.value--;
+    })
+    .addCase(incrementByAmount, (state, action) => {
+      state.value += action.payload;
+    });
+});
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+configureStore({
+  reducer: counterReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  // preloadedState,
+  // enhancers
+});
 
 const root = document.getElementById("root");
-render(<App />, root);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  root
+);
