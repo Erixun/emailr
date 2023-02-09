75f859e93a9dda86498f98187d2a9bba6774f597
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-02-09 21:26:59 +0100

    functional to class component

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,4 +1,5 @@
 //Rendering layer control (React Router)
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
 // const Header = () => <h2>Header</h2>;
@@ -6,25 +7,29 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    //container class improves materialize styling
-    <div className="container">
-      <BrowserRouter>
-        {/* Collection of routes */}
-        <div>
-          {/* Header always visible, regardless of route */}
-          {/* Content customized based on sign-in status */}
-          <Header />
+class App extends Component {
+  componentDidMount(): void {}
+  // useEffect(() => {})
+  render() {
+    return (
+      //container class improves materialize styling
+      <div className="container">
+        <BrowserRouter>
+          {/* Collection of routes */}
+          <div>
+            {/* Header always visible, regardless of route */}
+            {/* Content customized based on sign-in status */}
+            <Header />
 
-          {/* Greedily matching, unless exact */}
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+            {/* Greedily matching, unless exact */}
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
