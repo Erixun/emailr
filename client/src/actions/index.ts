561a45064cde16447343c9341f20253d2a3d54e1
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-02-10 22:19:42 +0100

    refactor

--- client/src/actions/index.ts
+++ client/src/actions/index.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { Dispatch } from "redux";
 import { FETCH_USER } from "./types";
 
@@ -9,11 +9,13 @@ import { FETCH_USER } from "./types";
  * Our first action creator, for fetching user on startup
  * @returns A function.
  */
-export const fetchUser = () => {
-  return (dispatch: Dispatch) => {
-    axios
-      .get("/api/current_user")
-      .then((res) => dispatch({ type: FETCH_USER, payload: res }))
-      .catch((err) => console.log(err));
-  };
+export const fetchUser = () => async (dispatch: Dispatch) => {
+  let res = undefined;
+  try {
+    res = await axios.get("/api/current_user");
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (res) dispatch({ type: FETCH_USER, payload: res });
 };
--- client/src/reducers/authReducer.ts
+++ client/src/reducers/authReducer.ts
@@ -1,17 +1,23 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
+import { FETCH_USER } from "../actions/types";
 
-const increment = createAction<number>("increment");
+// const increment = createAction<number>("increment");
 
-export const authReducer2 = createReducer({ counter: 0 }, (builder) => {
-  builder.addCase(increment, (state, action) => {
-    // action is inferred correctly here
-    state.counter += action.payload;
-  });
-});
+// export const authReducer2 = createReducer({ counter: 0 }, (builder) => {
+//   builder.addCase(increment, (state, action) => {
+//     // action is inferred correctly here
+//     state.counter += action.payload;
+//   });
+// });
 
-const authReducer = function (state = {}, action: { type: string }) {
-  console.log(action);
+const authReducer = function (
+  state = null,
+  action: { type: string; payload: any }
+) {
+  // console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
