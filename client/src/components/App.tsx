862ff09968f8c3bff3e88f07fb72dd2138209b99
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-02-12 21:53:53 +0100

    add type

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -10,9 +10,8 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 // const Landing = () => <h2>Landin g</h2>;
 
-class App extends Component {
+class App extends Component<{ fetchUser: Function }> {
   componentDidMount(): void {
-    //@ts-ignore
     this.props.fetchUser();
   }
   // useEffect(() => {})
