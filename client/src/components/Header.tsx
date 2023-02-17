cbfc129398dcfa57dcdeaccc1c5beb1e8b2be222
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-02-17 22:02:16 +0100

    create & include Payments

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,31 @@
 import { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component<{ auth: any; user?: any }> {
   renderContent() {
     const auth = this.props.auth;
-    return auth ? (
-      <li>
-        <a href="/api/logout">Logout?</a>
-      </li>
-    ) : auth === false ? (
-      <li>
-        <a href="/auth/google">Login With Google</a>
-      </li>
-    ) : null;
+    console.log(auth);
+    switch (auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return [
+          <li key={1}>
+            <Payments />
+          </li>,
+          <li key={2}>
+            <a href="/api/logout">Logout?</a>
+          </li>,
+        ];
+    }
   }
   render() {
     console.log(this.props);
