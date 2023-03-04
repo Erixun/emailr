4c0b3706984ab9b5013cef913a54acd51fe50ee7
Erik Sundberg <erik.sund.berg20@hotmail.se>
2023-03-04 19:07:19 +0100

    Update surveyRoutes.ts

--- routes/surveyRoutes.ts
+++ routes/surveyRoutes.ts
@@ -33,7 +33,6 @@ router.post("/api/surveys", requireLogin, requireCredits, (req, res, next) => {
   // 1. User gets sent to their destination.
   // 2. Sendgrid sends msg to our server informing us of the click.
 
-  //Emailr-key: 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
   res.send("unfinished route");
 });
 router.get("/api/surveys", requireLogin, (req, res, next) => {
