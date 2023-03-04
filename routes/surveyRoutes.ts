f72632865ad2a7b9c9ca60d10e876d0f0db21b3a
Erik Sundberg <erik.sundberg@unicus.com>
2023-03-04 18:51:34 +0100

    add comments

--- routes/surveyRoutes.ts
+++ routes/surveyRoutes.ts
@@ -6,9 +6,11 @@ import { SurveyModel } from "../models/Survey.js";
 
 const router = Router();
 const Survey = mongoose.model<SurveyModel>("surveys");
+// Creates a new survey and sends out emails in batch
 router.post("/api/surveys", requireLogin, requireCredits, (req, res, next) => {
   const { title, subject, body, recipient } = req.body;
 
+  //Survey instance
   const survey = new Survey({
     title,
     subject,
@@ -19,6 +21,19 @@ router.post("/api/surveys", requireLogin, requireCredits, (req, res, next) => {
     _user: req.user?.id,
     dateSent: Date.now(),
   });
+
+  //Email template
+
+  // Mailer - Sendgrid scans each email,
+  //          replacing every link with their own.
+  // They know the recipient of every email and links injected
+  // into an email contains a token to identify the user.
+
+  //Link-click effect:
+  // 1. User gets sent to their destination.
+  // 2. Sendgrid sends msg to our server informing us of the click.
+
+  //Emailr-key: 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
   res.send("unfinished route");
 });
 router.get("/api/surveys", requireLogin, (req, res, next) => {
