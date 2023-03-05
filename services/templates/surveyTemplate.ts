f23dea7bd04d8fa3869335d32c868bfdb1b48ee1
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2023-03-05 21:05:21 +0100

    fix & test template

--- services/templates/surveyTemplate.ts
+++ services/templates/surveyTemplate.ts
@@ -1,4 +1,18 @@
 const surveyTemplate = ({ body }: { body?: string }) =>
-  `<div>${body ?? "No content"}</div>`;
+  `<html>
+  <body>
+    <div style="text-align: center;">
+      <h3>I'd like your input!</h3>
+      <p>Please answer the following question:</p>
+      <p>${body}</p>
+      <div>
+        <a href="http://localhost:3000">Yes</a>
+      </div>
+      <div>
+        <a href="http://localhost:3000">No</a>
+      </div>
+    </div>
+  </body>
+  </html>`;
 
 export default surveyTemplate;
