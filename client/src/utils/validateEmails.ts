9e0bfc44ab44c665f28cba1f21154586fbae0e2a
Erik Sundberg <erik.sundberg@unicus.com>
2023-03-11 18:48:57 +0100

    create validateEmails

---
+++ client/src/utils/validateEmails.ts
@@ -0,0 +1,16 @@
+const validateEmails = (emailCsv: string) => {
+  const emails = emailCsv.split(",").map((email) => email.trim());
+  const invalidEmails = emails.filter((email) => !validateEmail(email));
+  if (invalidEmails.length) {
+    return `These emails are invalid: ${invalidEmails}`;
+  }
+  return false;
+};
+
+const validateEmail = (email: string) => {
+  // regex for email validation, \S+ means one or more non-whitespace characters
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
+
+export default validateEmails;
