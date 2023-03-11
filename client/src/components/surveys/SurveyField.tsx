02fe20fc9f859df165d775a467a7fed78f6a596f
Erik Sundberg <erik.sundberg@unicus.com>
2023-03-11 18:48:13 +0100

    refactor SurveyField

--- client/src/components/surveys/SurveyField.tsx
+++ client/src/components/surveys/SurveyField.tsx
@@ -1,15 +1,31 @@
 //create a survey field component with typescript typed parameters
 
+//Alternative way to destructure props
+// const SurveyField = (props: any) => {
+//   console.log(props);
+//   const {
+//     input,
+//     label,
+//     type,
+//     meta: { error, touched },
+//   } = props;
 const SurveyField = ({
   input,
   label,
+  type,
   meta: { error, touched },
 }: FieldProps) => {
+  console.log(input);
+  console.log(label);
+  console.log(error);
+
   return (
     <div>
       <label>{label}</label>
-      <input {...input} style={{ marginBottom: "5px" }} />
-      <div className="red-text" style={{ marginBottom: "20px" }}>
+      {/* {...input} hydrates the input tag with event handlers, eg onBlur, onChange etc */}
+      <input {...input} type={type} style={{ marginBottom: "5px" }} />
+      <div className="red-text text-darken-4" style={{ marginBottom: "20px" }}>
+        {/* if touched, show error (or "") */}
         {touched && error}
       </div>
     </div>
@@ -19,6 +35,7 @@ const SurveyField = ({
 type FieldProps = {
   input: any;
   label: string;
+  type: string;
   meta: { error: string; touched: boolean };
 };
 
