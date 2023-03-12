04263a8b70a38452aecc35c7303c9894be0e81b4
Erik Sundberg <erik.sundberg@unicus.com>
2023-03-12 18:29:15 +0100

    separate const FormFields

---
+++ client/src/components/surveys/FormFields.ts
@@ -0,0 +1,8 @@
+const FormFields = [
+  { label: "Survey Title", name: "title", type: "text" },
+  { label: "Subject Line", name: "subject", type: "text" },
+  { label: "Email Body", name: "body", type: "text" },
+  { label: "Recipient List", name: "recipients", type: "email" },
+];
+
+export default FormFields;
